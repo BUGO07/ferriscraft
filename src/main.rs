939a44add118aa3f54e3903c3a98d05bf5b04c6b
BUGO07 <71205861+BUGO07@users.noreply.github.com>
2025-07-23 14:38:54 +0400

    chunk unloading

--- src/main.rs
+++ src/main.rs
@@ -54,7 +54,15 @@ fn main() {
         ))
         .init_resource::<MovementSettings>()
         .add_systems(Startup, setup)
-        .add_systems(Update, (generate_chunks, apply_chunk_mesh))
+        .add_systems(
+            Update,
+            (
+                handle_chunk_gen,
+                handle_chunk_despawn,
+                process_tasks,
+                apply_chunk_mesh,
+            ),
+        )
         .run();
 }
 
@@ -66,15 +74,15 @@ pub struct GameInfo {
     pub materials: Vec<Handle<StandardMaterial>>,
 }
 
-#[derive(Resource, Reflect, Default)]
+#[derive(Reflect, Resource, Default)]
 pub struct GameSettings {
     pub movement_speed: f32,
+    pub render_distance: i32,
 }
 
 pub const CHUNK_SIZE: i32 = 16; // MAX 63
 pub const CHUNK_HEIGHT: i32 = 256; // MAX 511
 pub const SEA_LEVEL: i32 = 64;
-pub const RENDER_DISTANCE: i32 = 16;
 
 fn setup(
     mut commands: Commands,
@@ -93,6 +101,7 @@ fn setup(
     });
     commands.insert_resource(GameSettings {
         movement_speed: 200.0,
+        render_distance: 16,
     });
 
     commands.spawn((
@@ -118,43 +127,23 @@ fn setup(
     ));
 }
 
-fn generate_chunks(
+pub fn handle_chunk_gen(
     mut commands: Commands,
     mut movement_settings: ResMut<MovementSettings>,
-    mut task_query: Query<(Entity, &mut ComputeChunk)>,
     game_info: Res<GameInfo>,
     game_settings: Res<GameSettings>,
-    // chunks: Query<(Entity, &Transform), With<ChunkEntity>>,
     player: Single<&Transform, With<Camera3d>>,
 ) {
     movement_settings.speed = game_settings.movement_speed;
     let pt = player.translation;
     let thread_pool = AsyncComputeTaskPool::get();
+    let render_distance = game_settings.render_distance;
 
-    // for chunk in game_info.chunks.read().unwrap().keys() {
-    //     // check if its far from the player and if it is despawn it
-    //     if (chunk.x + RENDER_DISTANCE < pt.x as i32 / CHUNK_SIZE)
-    //         || (chunk.x - RENDER_DISTANCE > pt.x as i32 / CHUNK_SIZE)
-    //         || (chunk.z + RENDER_DISTANCE < pt.z as i32 / CHUNK_SIZE)
-    //         || (chunk.z - RENDER_DISTANCE > pt.z as i32 / CHUNK_SIZE)
-    //     {
-    //         commands
-    //             .entity(
-    //                 chunks
-    //                     .iter()
-    //                     .find(|x| x.1.translation.as_ivec3() / CHUNK_SIZE == *chunk)
-    //                     .unwrap()
-    //                     .0,
-    //             )
-    //             .despawn();
-    //         game_info.chunks.write().unwrap().remove(chunk);
-    //     }
-    // }
     for chunk_z in
-        (pt.z as i32 / CHUNK_SIZE - RENDER_DISTANCE)..(pt.z as i32 / CHUNK_SIZE + RENDER_DISTANCE)
+        (pt.z as i32 / CHUNK_SIZE - render_distance)..(pt.z as i32 / CHUNK_SIZE + render_distance)
     {
-        for chunk_x in (pt.x as i32 / CHUNK_SIZE - RENDER_DISTANCE)
-            ..(pt.x as i32 / CHUNK_SIZE + RENDER_DISTANCE)
+        for chunk_x in (pt.x as i32 / CHUNK_SIZE - render_distance)
+            ..(pt.x as i32 / CHUNK_SIZE + render_distance)
         {
             let current_chunk_key = ivec3(chunk_x, 0, chunk_z);
 
@@ -191,30 +180,18 @@ fn generate_chunks(
                         for y in 0..CHUNK_HEIGHT {
                             chunk.blocks[vec3_to_index(ivec3(rela_x, y, rela_z))] = Block {
                                 kind: if y == 0 {
-                                    // Always bedrock at the very bottom of the world
                                     BlockKind::Grass
                                 } else if y < max_y {
-                                    // If 'y' is below the terrain's surface (max_y),
-                                    // assign Grass, Dirt, or Stone based on depth.
                                     match y {
-                                        // Grass layer: The very top block of the solid terrain
                                         _ if y > 150 => BlockKind::Stone,
                                         _ if y == max_y - 1 => BlockKind::Grass,
-                                        // Dirt layer: Typically 3 blocks below the surface, up to 1 block below grass
-                                        _ if y >= max_y - 4 => BlockKind::Dirt, // Covers y from max_y - 4 to max_y - 2
-                                        // Stone layer: Below the dirt layer, extending downwards
-                                        _ => BlockKind::Stone, // Covers y < max_y - 4
+                                        _ if y >= max_y - 4 => BlockKind::Dirt,
+                                        _ => BlockKind::Stone,
                                     }
+                                } else if y < SEA_LEVEL {
+                                    BlockKind::Plank
                                 } else {
-                                    // If 'y' is at or above the terrain's surface (max_y),
-                                    // determine if it's air or bedrock (filling "water" areas).
-                                    if y < SEA_LEVEL {
-                                        // If below sea level, fill with Bedrock (mimicking water being replaced)
-                                        BlockKind::Plank
-                                    } else {
-                                        // If at or above sea level, it's Air
-                                        BlockKind::Air
-                                    }
+                                    BlockKind::Air
                                 },
                             };
                         }
@@ -227,29 +204,31 @@ fn generate_chunks(
                                     for (x, block) in tree_row.iter().enumerate() {
                                         let mut pos =
                                             ivec3(3 + x as i32, 1 + y as i32, 3 + z as i32);
-                                        // * shitty way of getting the ground height at the center of the tree
                                         let local_max_y = terrain_noise(
-                                            // &game_info.perlin,
                                             (chunk.pos * CHUNK_SIZE + pos).as_vec3().xz(),
                                             seed,
                                         );
 
-                                        pos.y += local_max_y;
+                                        pos.y += local_max_y as i32;
 
                                         if (0..CHUNK_SIZE).contains(&pos.x)
                                             && (0..CHUNK_HEIGHT).contains(&pos.y)
                                             && (0..CHUNK_SIZE).contains(&pos.z)
                                         {
                                             chunk.blocks[vec3_to_index(pos)] = *block;
-                                        } else {
-                                            // TODO this isn't a proper way to do it
-                                            chunks_for_task
+                                        } else if let Some(relative_chunk_key) =
+                                            chunk.get_relative_chunk(pos)
+                                            && let Some(target_chunk) = chunks_for_task
                                                 .write()
                                                 .unwrap()
-                                                .get_mut(&chunk.get_relative_chunk(pos).unwrap())
-                                                .unwrap()
-                                                .blocks
-                                                .insert(vec3_to_index(pos), *block);
+                                                .get_mut(&relative_chunk_key)
+                                        {
+                                            let block_index = vec3_to_index(
+                                                pos - relative_chunk_key * CHUNK_SIZE,
+                                            );
+                                            if block_index < target_chunk.blocks.len() {
+                                                target_chunk.blocks[block_index] = *block;
+                                            }
                                         }
                                     }
                                 }
@@ -263,33 +242,95 @@ fn generate_chunks(
             commands.spawn(ComputeChunk(task));
         }
     }
+}
 
+fn process_tasks(
+    mut commands: Commands,
+    mut spawn_tasks: Query<(Entity, &mut ComputeChunk)>,
+    mut despawn_tasks: Query<(Entity, &mut ComputeDespawn)>,
+    game_info: Res<GameInfo>,
+) {
     let mut processed_this_frame = 0;
-    for (entity, mut compute_task) in task_query.iter_mut() {
-        if processed_this_frame >= 50 {
+    for (entity, mut compute_task) in spawn_tasks.iter_mut() {
+        if processed_this_frame >= 250 {
             break;
         }
         if let Some(result) = future::block_on(future::poll_once(&mut compute_task.0)) {
             commands
                 .entity(entity)
-                .insert((
+                .try_insert((
                     ChunkEntity,
                     Transform::from_translation((result.1 * CHUNK_SIZE).as_vec3()),
                 ))
-                .remove::<ComputeChunk>();
+                .try_remove::<ComputeChunk>();
 
-            // Insert the completed chunk
             game_info.chunks.write().unwrap().insert(result.1, result.0);
-            // Remove from loading_chunks as it's now fully processed and inserted
             game_info.loading_chunks.write().unwrap().remove(&result.1);
 
             processed_this_frame += 1;
         }
     }
+
+    let mut processed_this_frame = 0;
+    for (task_entity, mut compute_task) in despawn_tasks.iter_mut() {
+        if processed_this_frame >= 1 {
+            break;
+        }
+        if let Some(despawn_list) = future::block_on(future::poll_once(&mut compute_task.0)) {
+            let mut game_chunks_write_guard = game_info.chunks.write().unwrap();
+            let mut game_loading_chunks_write_guard = game_info.loading_chunks.write().unwrap();
+            for (entity_to_despawn, chunk_key) in despawn_list {
+                commands.entity(entity_to_despawn).try_despawn();
+                game_chunks_write_guard.remove(&chunk_key);
+                game_loading_chunks_write_guard.remove(&chunk_key);
+            }
+            commands.entity(task_entity).try_despawn();
+            processed_this_frame += 1;
+        }
+    }
+}
+
+fn handle_chunk_despawn(
+    mut commands: Commands,
+    game_settings: Res<GameSettings>,
+    chunks_query: Query<(Entity, &Transform), With<ChunkEntity>>,
+    player: Single<&Transform, With<Camera3d>>,
+) {
+    let pt = player.translation;
+    let thread_pool = AsyncComputeTaskPool::get();
+    let render_distance = game_settings.render_distance;
+
+    let mut chunks_to_check: Vec<(IVec3, Entity)> = Vec::new();
+    for (entity, transform) in chunks_query.iter() {
+        let chunk_key = transform.translation.as_ivec3() / CHUNK_SIZE;
+        chunks_to_check.push((chunk_key, entity));
+    }
+
+    if chunks_to_check.is_empty() {
+        return;
+    }
+
+    let task = thread_pool.spawn(async move {
+        let mut despawn_list: Vec<(Entity, IVec3)> = Vec::new();
+        for (chunk_key, entity) in chunks_to_check {
+            if (chunk_key.x + render_distance < pt.x as i32 / CHUNK_SIZE)
+                || (chunk_key.x - render_distance > pt.x as i32 / CHUNK_SIZE)
+                || (chunk_key.z + render_distance < pt.z as i32 / CHUNK_SIZE)
+                || (chunk_key.z - render_distance > pt.z as i32 / CHUNK_SIZE)
+            {
+                despawn_list.push((entity, chunk_key));
+            }
+        }
+        despawn_list
+    });
+    commands.spawn(ComputeDespawn(task));
 }
 
 #[derive(Component)]
 struct ComputeChunk(Task<(Chunk, IVec3)>);
+
+#[derive(Component)]
+struct ComputeDespawn(Task<Vec<(Entity, IVec3)>>);
 
 #[derive(Component)]
 struct ComputeChunkMesh(Task<Option<ChunkMesh>>);
@@ -320,7 +361,7 @@ fn apply_chunk_mesh(
             }
         });
 
-        commands.entity(entity).insert(ComputeChunkMesh(task));
+        commands.entity(entity).try_insert(ComputeChunkMesh(task));
     }
 
     let mut processed_this_frame = 0;
@@ -331,7 +372,7 @@ fn apply_chunk_mesh(
         }
 
         if let Some(result) = future::block_on(future::poll_once(&mut compute_task.0)) {
-            commands.entity(entity).remove::<ComputeChunkMesh>();
+            commands.entity(entity).try_remove::<ComputeChunkMesh>();
 
             if let Some(mesh_data) = result {
                 let mut bevy_mesh = Mesh::new(
@@ -354,7 +395,7 @@ fn apply_chunk_mesh(
 
                 let mesh_handle = meshes.add(bevy_mesh);
 
-                commands.entity(entity).insert((
+                commands.entity(entity).try_insert((
                     Mesh3d(mesh_handle),
                     MeshMaterial3d(game_info.materials[0].clone()),
                     Visibility::Visible,
